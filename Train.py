0efb9ec0147b0a046856884ee92bcd3708cee4d7
lus <lus@lamda.nju.edu.cn>
2021-06-09 16:27:29 +0800

    update basic codes

--- Train.py
+++ Train.py
@@ -19,7 +19,7 @@ from torch.nn import functional as F
 
 from pytorch_metric_learning.miners import TripletMarginMiner
 
-from Test import test
+from Test import test, test_ncm
 from utils.triplet import merge
 
 def pretrain(args, train_data_loader, test_data_loader, network, model_save_path):
@@ -85,6 +85,12 @@ def pretrain(args, train_data_loader, test_data_loader, network, model_save_path
 
 
 
+def train_stage1(args, train_data_loader, test_data_loader, teacher, student, model_save_path1):
+    # TODO
+    return
+
+
+
 def train_stage2(args, train_data_loader, test_data_loader, teacher, student, model_save_path2):
     training_loss_function = nn.CrossEntropyLoss()
     teaching_loss_function = nn.KLDivLoss(reduction='batchmean')
@@ -99,6 +105,7 @@ def train_stage2(args, train_data_loader, test_data_loader, teacher, student, mo
     teaching_loss_list2 = []
     training_accuracy_list2 = []
     testing_accuracy_list2 = []
+    best_testing_accuracy = 0
 
     for epoch in range(1, args.n_training_epochs2 + 1):
         training_loss = 0
@@ -114,10 +121,57 @@ def train_stage2(args, train_data_loader, test_data_loader, teacher, student, mo
             logits = student.forward(images)
             training_loss_value = training_loss_function(logits, labels)
 
-            if args.model_name == 'kd':
+            if args.model_name == 'ce':
+                pass
+            elif args.model_name == 'kd':
                 with torch.no_grad():
                     teacher_logits = teacher.forward(images)
-                    teaching_loss_value = teaching_loss_function()
+                    teaching_loss_value = args.lambd * teaching_loss_function(
+                        F.log_softmax(logits / args.tau2, dim=1),
+                        F.softmax(teacher_logits / args.tau2, dim=1)
+                    )
+            elif args.model_name == 'gkd':
+                # TODO
+                pass
 
+            total_loss_value = training_loss_value + teaching_loss_value
 
-            # training_loss += training_loss_value.
+            optimizer.zero_grad()
+            total_loss_value.backward()
+            optimizer.step()
+
+            prediction = torch.argmax(logits, dim=1)
+            training_loss += training_loss_value.cpu().item() * images.size()[0]
+            if args.model_name in {'kd', 'gkd'}:
+                teaching_loss += teaching_loss_value.cpu().item() * images.size()[0]
+            else:
+                pass
+            training_accuracy += torch.sum((prediction == labels).float()).cpu().item()
+        
+        training_loss /= train_data_loader.dataset.__len__()
+        training_loss_list2.append(training_loss)
+        teaching_loss /= train_data_loader.dataset.__len__()
+        teaching_loss_list2.append(teaching_loss)
+        training_accuracy /= train_data_loader.dataset.__len__()
+        training_accuracy_list2.append(training_accuracy)
+        testing_accuracy = test(args, test_data_loader, student)
+        testing_accuracy_list2.append(testing_accuracy)
+
+        print('epoch %d finish: training_loss = %f, teaching_loss = %f, training_accuracy = %f, testing_accuracy = %f' % (
+            epoch, training_loss, teaching_loss, training_accuracy, testing_accuracy
+        ))
+
+        # if we find a better model
+        if not args.flag_debug:
+            if testing_accuracy > best_testing_accuracy:
+                best_testing_accuracy = testing_accuracy
+                record = {
+                    'state_dict': student.state_dict(),
+                    'testing_accuracy': testing_accuracy,
+                    'epoch': epoch
+                }
+                torch.save(record, model_save_path2)
+
+        scheduler.step()
+
+        return training_loss_list2, teaching_loss_list2, training_accuracy_list2, testing_accuracy_list2
